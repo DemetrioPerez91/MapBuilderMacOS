4eb7d9c6d5d8263951cc6940ac8e3d6327b34436
Demetrio <dem3perez@gmail.com>
2017-09-29 07:06:34 -0400

    set up SDL in  XCode

--- MapBuilderPort/MapBuilderPort/main.cpp
+++ MapBuilderPort/MapBuilderPort/main.cpp
@@ -7,9 +7,16 @@
 //
 
 #include <iostream>
+#include <SDL2/SDL.h>
 
 int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello, World!\n";
+    if (SDL_Init(SDL_INIT_EVERYTHING)<0)
+    {
+       std::cout <<SDL_GetError();
+    }
+
+    
     return 0;
 }
