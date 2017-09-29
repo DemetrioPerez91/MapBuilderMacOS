aa6eb183cdd2b32ad2ab16cb77dab76cddf2378e
Demetrio <dem3perez@gmail.com>
2017-09-29 07:43:59 -0400

    Add Tile.h

---
+++ MapBuilderPort/MapBuilderPort/Tile.cpp
@@ -0,0 +1,40 @@
+#include "stdafx.h"
+#include "Tile.h"
+
+
+Tile::Tile()
+{
+	
+}
+
+void Tile::setTileSize(int size)
+{
+	setSourceRect(0, 0, 16, 16);
+	setDestRect(DestRect->x,DestRect->y,size,size);
+	tileSize = size;
+}
+
+int Tile::getTileWidth()
+{
+	return DestRect->w;
+}
+
+int Tile::getTileHeight()
+{
+	return DestRect->y;
+}
+
+void Tile::setTileCoordinates(int x, int y)
+{
+	DestRect->x = x;
+	DestRect->y = y;
+}
+
+void Tile::setTile( int new_tileType)
+{
+	tileType = new_tileType;
+}
+
+Tile::~Tile()
+{
+}
---
+++ MapBuilderPort/MapBuilderPort/Tile.h
@@ -0,0 +1,22 @@
+#pragma once
+#include "GameObject.h"
+class Tile:public GameObject
+{
+public:
+	Tile();
+	~Tile();
+	int tileSize;
+	void setTileSize(int size);
+	ObjectType getType()
+	{
+		return  TILE;
+	}
+	int tileType;
+	int getTileWidth();
+	int getTileHeight();
+	void setTileCoordinates(int x,int y);
+	void OnCollision(GameObject * other){}
+	void setTile(int setTile);
+	
+};
+
